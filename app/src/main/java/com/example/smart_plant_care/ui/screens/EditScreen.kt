48e9536fe5b4806a0628dcc817cd0c65d61a1fb9
stasiand <stasiand@cvut.cz>
2026-04-14 00:31:35 +0200

    remove unusable imports

--- app/src/main/java/com/example/smart_plant_care/ui/screens/EditScreen.kt
+++ app/src/main/java/com/example/smart_plant_care/ui/screens/EditScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
